b8ebddfc30f1bdaef70c135eeb7a24ff0d0b40ae
Daniel García Molero <dangar13@ucm.es>
2017-03-13 16:51:00 +0100

    116
    116 solved

---
+++ 116/116/116.cpp
@@ -0,0 +1,19 @@
+//
+//  116.cpp
+//  116
+//
+//  Created by Daniel García Molero on 13/03/2017.
+//  Copyright © 2017 Daniel García Molero. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << "Hola mundo." << endl;
+    }
+    return 0;
+}
