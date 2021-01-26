3dc814b000b8f1e794135c352bf810166a1cd42f
Daniel G <dangar13@icloud.com>
2021-01-26 22:10:57 +0100

    Corrected and renamed Two Sum exercise

--- LeetCode/0001 - Two Sum.cpp
+++ LeetCode/0001 - Two Sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> neededSet; // Value, index
+        unordered_map<int, int> neededSet; // Needed value, index
         for (size_t i = 0; i < nums.size(); ++i) {
             int needed = target - nums[i];
-            if (neededSet.find(nums[i]) != neededSet.end()) {
+            if (neededSet.find(needed) != neededSet.end()) {
                 vector<int> result;
                 result.push_back(neededSet[needed]);
                 result.push_back(i);
                 return result;
             } else {
-                neededSet.insert(make_pair(target - nums[i], i));
+                neededSet.insert(make_pair(nums[i], i));
             }
         }
     
-        return nums; // There should always be a solution
+        return nums;
     }
 };
